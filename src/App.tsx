4834b53e3a38df5b2187bcb54e0052166606e1a2
cutpix <devdice@outlook.com>
2023-05-20 02:32:42 +0300

    Change VideoBg

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { ProfilePage } from './pages'
 
-import BackgroundVideo from './assets/video/background.mp4'
+import videoBg from './assets/video/purple-tech-bg.mp4'
 import styles from './App.module.scss'
 
 const App = () => {
 
   return (
     <div className={`${styles['animation-background']} ${styles.overlay}}`}>
-      <video className={styles.overlay} src={BackgroundVideo} autoPlay loop muted />
+      <video className={styles.overlay} src={videoBg} autoPlay loop muted />
       <Routes>
         <Route path='/' element={<ProfilePage />} />
         <Route path='/*' element={<Navigate to='/' replace />} />
