e7c6c845a4fcb1e411cc5e0182fc307cdb49808a
cutpix <devdice@outlook.com>
2023-05-20 02:20:40 +0300

    Video Bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { ProfilePage } from './pages'
 
+import BackgroundVideo from './assets/video/background.mp4'
 import styles from './App.module.scss'
 
 const App = () => {
 
   return (
-    <div className={styles['animation-background']}>
+    <div className={`${styles['animation-background']} ${styles.overlay}}`}>
+      <video className={styles.overlay} src={BackgroundVideo} autoPlay loop muted />
       <Routes>
         <Route path='/' element={<ProfilePage />} />
         <Route path='/*' element={<Navigate to='/' replace />} />
