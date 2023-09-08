f492f2d55bd1d710d9cf7cc3e7e1f1c2c495bf46
Andrii Pavliuk <devdice@outlook.com>
2023-09-09 02:40:17 +0300

    Clean nextjs app.

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Andrii Pavliuk | vCard / Resume / CV',
+  description: 'I am Andrii Pavliuk, full-stack software engineer from Ukraine, Kiev.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
