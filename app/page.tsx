f143d241e512b935ee99194f0d0de5175b895e60
Andrii Pavliuk <devdice@outlook.com>
2023-09-09 02:45:25 +0300

    Environment Setup
    Remove default tags in home page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1 className='text-3xl font-bold text-indigo-500'>Home Page</h1>
     </main>
   )
 }
