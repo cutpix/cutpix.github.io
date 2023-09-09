5c7b63659737d9c252ae844d973503c248a36311
Andrii Pavliuk <devdice@outlook.com>
2023-09-09 07:34:58 +0300

    Added Clerk for auth functionality.

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,16 +1,11 @@
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className='flex flex-col'>
-      <h1 className='text-3xl font-bold text-indigo-500'>Home Page</h1>
-      <p>
-        This is a protected route.
-      </p>
-      <Link href='/profile'>
-        Go to Profile Page
-      </Link>
-    </main>
+    <div>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   )
 }
