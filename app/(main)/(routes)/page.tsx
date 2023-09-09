1ec33dbd9557e9a8d857282f72011ecf08418c9e
Andrii Pavliuk <devdice@outlook.com>
2023-09-09 06:53:51 +0300

    Structure folderss.

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function Home() {
   return (
     <main className='flex flex-col'>
       <h1 className='text-3xl font-bold text-indigo-500'>Home Page</h1>
+      <p>
+        This is a protected route.
+      </p>
+      <Link href='/profile'>
+        Go to Profile Page
+      </Link>
     </main>
   )
 }
