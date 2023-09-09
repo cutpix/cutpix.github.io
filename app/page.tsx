174e6c2df98cc5574b6cbfb1b9ad00045b5f54f7
Andrii Pavliuk <devdice@outlook.com>
2023-09-09 04:52:19 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,6 @@ export default function Home() {
   return (
     <main className='flex flex-col'>
       <h1 className='text-3xl font-bold text-indigo-500'>Home Page</h1>
-      <br />
-      <div>
-        <Button variant="ghost">
-          Click Me
-        </Button>
-      </div>
     </main>
   )
 }
