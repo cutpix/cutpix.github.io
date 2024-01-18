9b2df80466ba8e3ba0a47c3d188bdff161786e36
Andrii Pavliuk <devdice@outlook.com>
2024-01-18 21:44:00 +0200

    Update

--- app/(profile)/page.tsx
+++ app/(profile)/page.tsx
@@ -1,10 +1,10 @@
-import { Header, Content, Title } from './common'
+import { Header, Content, Title, Row, Column, TextBlock } from './common'
 
 import CardStarted from "./components/CardStarted"
 import CardInner from "./components/CardInner"
 
 const ProfilePage = () => {
-
+  const data = getProfileInfo()
 
   return (
     <>
@@ -14,12 +14,26 @@ const ProfilePage = () => {
 
         <Content name='about'>
           <Title text='About Me' />
-          
+          <Row>
+            <Column className='col-d-6 border-line-v'>
+              <TextBlock text={data.description} />
+            </Column>
+          </Row>
         </Content>
 
       </CardInner>
     </>
   )
+}
+
+const getProfileInfo = () => {
+  const description = `I am Andrii Pavliuk, full-stack software engineer from Ukraine, Kyiv.
+  I make the web a more interactive and exciting place by creating applications
+  for the modern 🌍 world. I love working with the latest technologies
+  to build modular and testable projects.`
+
+  // Pass the data as props
+  return { description }
 }
 
 export default ProfilePage
